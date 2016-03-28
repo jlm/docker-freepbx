291f11e792e3ff5979c089634def99a5f17757de
Dave Oxley <dave@daveoxley.co.uk>
2016-03-28 23:37:13 +1100

    Persist asterisk configuration between restarts.

--- start-asterisk.sh
+++ start-asterisk.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ ! -d /conf/asterisk ]]; then
+    mv /etc/asterisk /conf/asterisk
+else
+    rm -rf /etc/asterisk
+fi
+ln -sf /conf/asterisk /etc/asterisk
+
 /usr/sbin/asterisk -f
 
