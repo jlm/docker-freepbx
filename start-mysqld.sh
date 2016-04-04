9ebafc212624021f27cab854e3fd896334712206
Dave Oxley <dave@daveoxley.co.uk>
2016-04-05 00:12:08 +1000

    Make freepbx.conf persistent.

--- start-mysqld.sh
+++ start-mysqld.sh
@@ -2,9 +2,12 @@
 
 if [[ ! -d /conf/mysql ]]; then
     mv /var/lib/mysql /conf/mysql
+    mv /etc/freepbx.conf /conf
 else
     rm -rf /var/lib/mysql
+    rm -f /etc/freepbx.conf
 fi
 ln -sf /conf/mysql /var/lib/mysql
+ln -sf /conf/freepbx.conf /etc/freepbx.conf
 
 exec mysqld_safe
