bc1090704c6fc19d61b5c343dec60081cb381bfa
Dave Oxley <dave@daveoxley.co.uk>
2016-04-09 11:35:40 +1000

    Add support for custom asterisk config.

--- start-asterisk.sh
+++ start-asterisk.sh
@@ -7,6 +7,12 @@ else
 fi
 ln -sf /conf/asterisk_spool /var/spool/asterisk
 
+if [[ -d /conf/asterisk ]]; then
+    for i in `ls /conf/asterisk/*_custom.conf`; do
+        ln -sf /conf/asterisk/$(basename $i) /etc/asterisk/$(basename $i)
+    done
+fi
+
 sleep 5 && fwconsole reload &
 
 /usr/sbin/asterisk -f
