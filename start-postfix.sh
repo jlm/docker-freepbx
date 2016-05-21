25c001713343eceda5a73788f61e3e77e3a94a88
Dave Oxley <dave@daveoxley.co.uk>
2016-05-21 12:38:45 +1000

    Upgrade freepbx and asterisk. Also link to postfix config in /conf.

--- start-postfix.sh
+++ start-postfix.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+if [[ ! -f /conf/postfix/main.cf ]]; then
+    mkdir /conf/postfix
+    mv /etc/postfix/main.cf /conf/postfix
+else
+    rm -f /etc/postfix/main.cf
+fi
+ln -sf /conf/postfix/main.cf /etc/postfix/main.cf
+
 /usr/lib/postfix/master -c /etc/postfix/ -d
